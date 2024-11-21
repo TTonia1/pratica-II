83af12bd55e13f90c7013e8f1bdb35f9d193f438
TTonia1 <161315439+TTonia1@users.noreply.github.com>
2024-11-21 08:31:53 -0300

    banco de dados

---
+++ praticaII_db.sql
@@ -0,0 +1,28 @@
+create database pratica2;
+use pratica2;
+
+create table cliente(
+id_cliente int not null primary key auto_increment,
+nome varchar(45),
+CPF char(11),
+email varchar(50),
+telefone char(11)
+);
+
+create table funcionario(
+id_funcionario int not null primary key auto_increment,
+nome varchar(45),
+email varchar(50)
+);
+
+create table solicitacao(
+id_solicitacao int not null primary key auto_increment,
+descricao varchar(200),
+urgencia varchar(5),
+status_solicitacao varchar(20),
+data_abertura date,
+id_cliente int not null,
+foreign key(id_cliente) references cliente(id_cliente),
+id_funcionario int,
+foreign key (id_funcionario) references funcionario(id_funcionario)
+);
